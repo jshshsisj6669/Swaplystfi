85c7ff9ac836ef40342e324652f05faf6657afc2
jshshsisj6669 <blitzchain4@gmail.com>
2025-05-14 05:49:20 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ['@rainbow-me/rainbowkit', 'wagmi'],
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
