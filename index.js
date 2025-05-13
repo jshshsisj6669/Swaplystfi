0e9980b4c6dc0ecaafa61e78feb03edcd54ea58a
jshshsisj6669 <blitzchain4@gmail.com>
2025-05-13 23:49:32 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to SwaplystFi</h1>
+      <p>Powered by Uniswap. Built for Multichain Freedom.</p>
+    </div>
+  );
+}
