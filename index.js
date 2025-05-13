cf754d5d9adaeb881354724574688ff104cee532
jshshsisj6669 <blitzchain4@gmail.com>
2025-05-13 23:51:55 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Welcome to SwaplystFi</h1>
-      <p>Powered by Uniswap. Built for Multichain Freedom.</p>
-    </div>
-  );
-}
